d18b0ee741e0a14dbe8de0fa3f5651cb8edaf9c7
Massimiliano <91194651+Massimiliano-HA@users.noreply.github.com>
2024-03-01 10:13:52 +0100

    Adding redux persist for user data, fixing app icons

--- src/components/navigators/navigators.style.ts
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "react-native";
-
-export const styles = StyleSheet.create({
-  tabBarContainer: {
-    flexDirection: "column",
-    flex: 1,
-    backgroundColor: "black",
-  },
-});
